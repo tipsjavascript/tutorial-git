d9070a0206af1cab6bef320c21e9d256252e93e0
anonystick <tungns@queenb.vn>
2021-09-28 13:12:40 +0700

    C add tiger fucntion

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const catClass = (name) => {
     return name;
 }
 
+
+// C add class Tiger
+
+const tigerClass = (name) => {
+    
+    return name;
+}
+
+
+
 module.exports = {
     dogClass,
-    catClass
+    catClass,
+    tigerClass
 }
