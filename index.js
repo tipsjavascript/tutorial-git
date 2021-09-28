caaa207c4eb57d8dc107ba636a61348d66595879
anonystick <tungns@queenb.vn>
2021-09-28 13:06:13 +0700

    B add cat function to index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const dogClass = (name) => {
     return name;
 }
 
+// B add class Cat
+
+const catClass = (name) => {
+    
+    return name;
+}
+
 module.exports = {
-    dogClass
+    dogClass,
+    catClass
 }
